1a8f56021c4b3b332adda95df04efd7bc295a7f6
Abdulrofihi Musa <abdulrofihimusa@gmail.com>
2025-09-11 17:12:07 +0100

    feat: add delta time

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ BLACK = (0, 0, 0)
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(BLACK)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
